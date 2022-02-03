e15a9531a0d51b11bb44076510442c5d89127262
Leandromarinh <leandrinm14@gmail.com>
2022-02-03 12:44:47 -0300

    estruturando as pastas

---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+export default {
+   background: "#fff",
+   fluxo: "#FA7D21",
+   error: "#F02849",
+   placeholder: rgba(0, 0, 0, .5),
+   inputColor: rgba(195, 197, 199, 0.2),
+   defaultText: "#000"
+}
